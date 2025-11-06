34c86f86744e520336ea410146ba7c72ec19f20d
Prajwal Bashyal <bashyalprajwal24@gmail.com>
2025-11-06 15:49:58 +0545

    real_time_clock

---
+++ JavaScript/real_time_clock.js
@@ -0,0 +1,9 @@
+function updateClock() {
+      let now = new Date();
+      let h = now.getHours().toString().padStart(2, '0');
+      let m = now.getMinutes().toString().padStart(2, '0');
+      let s = now.getSeconds().toString().padStart(2, '0');
+      document.getElementById("clock").innerText = h + ":" + m + ":" + s;
+    }
+    setInterval(updateClock, 1000);
+    updateClock();
